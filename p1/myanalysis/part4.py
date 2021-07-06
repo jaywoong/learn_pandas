91813fa20817f43d16f7c964101f862bdac688de
LJaeung <jaewoong9192@gmail.com>
2021-07-06 11:25:25 +0900

    part4

--- p1/myanalysis/part4.py
+++ p1/myanalysis/part4.py
@@ -16,8 +16,29 @@ class P109:
         df_seoul.set_index('전입지', inplace=True)
         sr_one = df_seoul.loc['경기도']
         print(sr_one)
+        plt.plot(sr_one)
+        plt.title('서울 -> 경기')
         plt.plot(sr_one.index, sr_one.values)
         plt.show()
 
+    def mat02(self):
+        df2 = df.fillna(method='ffill')
+        print(df2)
+        mask = (df2['전출지별'] == '서울특별시') & (df['전입지별'] != '서울특별시')
+        df_seoul = df2[mask]
+        df_seoul = df_seoul.drop(['전출지별'], axis=1)
+        df_seoul.rename({'전입지별':'전입지'}, axis=1, inplace=True)
+        df_seoul.set_index('전입지', inplace=True)
+        df3 = df_seoul.loc[['경상북도','강원도','충청북도','전라남도']]
+        print(df3)
+
+
+        # df3t = df3.T
+        # print(df3t)
+        # plt.style.use('ggplot')
+        # df3t.index = df3t.index.map(int)
+        # df3t.plot(kind='area', stacked=False, alpha=0.2, figsize=(10,5))
+        # plt.show()
+
 if __name__ == '__main__':
-    P109().mat01();
+    P109().mat02();
